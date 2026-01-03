d94601d123928993a6399fb471a433950c239bb5
Kojib <123880127+kojibai@users.noreply.github.com>
2026-01-03 12:43:23 -0500

    Use phi.svg icons in vault/position UI and prevent time truncation

--- src/SigilMarkets/views/Vault/VaultSigilCard.tsx
+++ src/SigilMarkets/views/Vault/VaultSigilCard.tsx
@@ -6,7 +6,7 @@ import type { KaiMoment } from "../../types/marketTypes";
 import type { VaultRecord } from "../../types/vaultTypes";
 import { Card, CardContent } from "../../ui/atoms/Card";
 import { Chip } from "../../ui/atoms/Chip";
-import { Icon } from "../../ui/atoms/Icon";
+import { PhiIcon } from "../../ui/atoms/PhiIcon";
 import { Button } from "../../ui/atoms/Button";
 import { shortHash, shortKey } from "../../utils/format";
 import { useSigilMarketsUi } from "../../state/uiStore";
@@ -55,7 +55,7 @@ export const VaultSigilCard = (props: VaultSigilCardProps) => {
           </div>
 
           <div className="sm-vault-sigil-badges">
-            <Chip size="sm" selected={false} variant="outline" tone={statusTone(v.status)} left={<Icon name="vault" size={14} tone="dim" />}>
+            <Chip size="sm" selected={false} variant="outline" tone={statusTone(v.status)} left={<PhiIcon size={14} />}>
               {v.status}
             </Chip>
             <Chip size="sm" selected={false} variant="outline" left={<Icon name="spark" size={14} tone="dim" />}>
