b79ac7da6ca7f6aa367824858fdad5bada88dff9
Kojib <kojibchat@gmail.com>
2026-01-05 14:48:15 -0500

    v2.0.1
    Added Version Control to Footer

--- src/config/buildInfo.ts
+++ src/config/buildInfo.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = "Verahai";
-export const APP_VERSION = "1.0.0";
-export const GITHUB_REPO_URL = "https://github.com/kojibai/sigilmarkets";
+export const APP_VERSION = "2.0.1";
+export const GITHUB_REPO_URL = "https://github.com/phinetwork/verahai";
 
 export const GITHUB_RELEASE_URL = (version: string): string =>
-  `https://github.com/kojibai/sigilmarkets/releases/tag/v${version}`;
+  `https://github.com/phinetwork/verahai/releases/tag/v${version}`;
